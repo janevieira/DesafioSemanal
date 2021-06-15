7fcbc76bd1c597217201223cd4f2b89286dcafda
Jane Vieira <janevieirapinto@gmail.com>
2021-06-15 11:43:26 -0300

    Correção realizada

--- tarefa-12.rb
+++ tarefa-12.rb
@@ -2,14 +2,15 @@
 def torreHanoi(discos)
     puts "Com #{discos} discos o jogo será encerrado com #{(2**discos)-1} jogadas."
 end
+entrada = 0
 
-puts "Qual o numero de discos na Torre de Hanói?" 
-entrada = gets.chomp.to_i
+while entrada == 0  
+    puts "Qual o numero de discos na Torre de Hanói?" 
+    entrada = gets.chomp.to_i
 
-if entrada == 0  
-    puts "Entrada INVÁLIDA"
-
-else 
-    torreHanoi(entrada)
-
+    if entrada  != 0
+        torreHanoi(entrada)
+    else 
+        puts "Entrada ZERO inválida."
+    end    
 end
